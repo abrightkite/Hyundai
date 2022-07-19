669b0e1afa2d7074a475763f9f22d40f467c0dc2
abrightkite <phy5397@gmail.com>
2022-07-20 06:33:43 +0900

    feat(Main): Component 추가

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import AdvertisementContainer from "../Advertisement/containers/AdvertisementContainer";
 import BannerContainer from "../Banner/containers/BannerContainer";
+import FaqContainer from "../Faq/containers/FaqContainer";
 import HeaderContainer from "../Header/containers/HeaderContainer";
 import IntroContainer from "../Intro/containers/IntroContainer";
+import PartnersContainer from "../Partners/containers/PartnersContainer";
 import ProductContainer from "../Product/containers/ProductContainer";
+import RoadMapContainer from "../RoadMap/containers/RoadMapContainer";
 import SaleContainer from "../Sale/containers/SaleContainer";
-import "./css/main.css";
+// import "./css/main.css";
 
 const Main = () => {
   return (
@@ -18,6 +21,9 @@ const Main = () => {
       <ProductContainer />
       <AdvertisementContainer />
       <SaleContainer />
+      <RoadMapContainer />
+      <PartnersContainer />
+      <FaqContainer />
     </div>
   );
 };
